9f1a2b00d569e14314c867731123fc10c43b2917
Nicoline L. Thomsen <xnlth@energinet.dk>
2024-08-23 14:46:26 +0200

    Added timing limits for following behaviour

--- face_following/face_following/face_following_node.py
+++ face_following/face_following/face_following_node.py
@@ -76,6 +76,10 @@ class FaceFollowingNode(Node):
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
 
         # Node variables
+        self.detection_time_stamp = self.get_clock().now()
+        self.detection_time_stop_error = 0.5  # Seconds
+        self.detection_time_stop_follow = 4.0  # Seconds
+
         self.center_x = round(self.image_w / 2)
         self.center_y = round(self.image_h / 2)
 
@@ -106,11 +110,31 @@ class FaceFollowingNode(Node):
         self.target_x = msg.center.position.x
         self.target_y = msg.center.position.y
 
+        self.detection_time_stamp = self.get_clock().now()
+
     def timer_callback(self):
 
-        error_pan = self.center_x - self.target_x
-        # error_tilt = self.center_y - self.target_y
+        time_current = self.get_clock().now()
+        time_diff = (
+            time_current - self.detection_time_stamp
+        ).nanoseconds / 1000000000  # Seconds
 
+        # Stop following if detection is gone for too long
+        if time_diff > self.detection_time_stop_follow:
+            self.servo_pan.reset_position()
+            # self.servo_tilt.reset_position()
+            return
+
+        # Reset error if detection is gone for too long
+        if time_diff > self.detection_time_stop_error:
+            error_pan = 0.0
+            error_tilt = 0.0
+
+        else:
+            error_pan = self.center_x - self.target_x
+            error_tilt = self.center_y - self.target_y
+
+        # Send command to servos
         self.servo_pan.compute_control(error_pan, self.timer_period)
         # self.servo_tilt.compute_control(error_tilt, self.timer_period)
 
