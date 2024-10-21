1486cd24dc40b69b04c1289b9d42a16d2aecafd3
Nicoline L. Thomsen <xnlth@energinet.dk>
2024-10-21 16:19:14 +0200

    Added teleoperation launch file

---
+++ elrik_bringup/launch/teleoperation.launch.py
@@ -0,0 +1,48 @@
+from launch import LaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+package_name = "elrik_bringup"
+
+
+def launch_setup(context, *args, **kwargs):
+
+    camera_model = "zed2i"
+    image_topic_left = "/zed/zed_node/left/image_rect_color/compressed"
+    image_topic_right = "/zed/zed_node/right/image_rect_color/compressed"
+
+    zed_camera_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("zed_wrapper"), "/launch", "/zed_camera.launch.py"]
+        ),
+        launch_arguments={"camera_model": camera_model}.items(),
+    )
+
+    teleoperation_node = Node(
+        package="teleoperation",
+        executable="teleoperation_node",
+        output="screen",
+        remappings=[
+            ("/image_left", image_topic_left),
+            ("/image_right", image_topic_right),
+        ],
+    )
+
+    return [
+        zed_camera_launch,
+        teleoperation_node,
+    ]
+
+
+def generate_launch_description():
+
+    return LaunchDescription(
+        [
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
