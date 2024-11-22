92b13e8f65e5be3fb38517820959df3e60fe6652
Nicoline L. Thomsen <xnlth@energinet.dk>
2024-11-22 15:07:28 +0100

    Added all arm nodes to launch

--- elrik_bringup/launch/arm.launch.py
+++ elrik_bringup/launch/arm.launch.py
@@ -39,6 +39,61 @@ def launch_setup(context, *args, **kwargs):
         parameters=[servo_params],
     )
 
+    servo_left_shoulder_roll_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_left/shoulder_roll",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_left_arm_yaw_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_left/arm_yaw",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_left_elbow_pitch_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_left/elbow_pitch",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_left_forearm_yaw_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_left/forearm_yaw",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_left_wrist_pitch_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_left/wrist_pitch",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_left_wrist_roll_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_left/wrist_roll",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    # Right Arm
     servo_right_shoulder_pitch_node = Node(
         package="servo_control",
         executable="servo_control_node",
@@ -48,12 +103,78 @@ def launch_setup(context, *args, **kwargs):
         parameters=[servo_params],
     )
 
+    servo_right_shoulder_roll_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_right/shoulder_roll",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_right_arm_yaw_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_right/arm_yaw",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_right_elbow_pitch_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_right/elbow_pitch",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_right_forearm_yaw_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_right/forearm_yaw",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_right_wrist_pitch_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_right/wrist_pitch",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
+    servo_right_wrist_roll_node = Node(
+        package="servo_control",
+        executable="servo_control_node",
+        namespace="arm_right/wrist_roll",
+        name="servo",
+        output="screen",
+        parameters=[servo_params],
+    )
+
     # Right Arm
 
     return [
         servo_driver_node,
-        servo_left_shoulder_pitch_node,
-        servo_right_shoulder_pitch_node,
+        # servo_left_shoulder_pitch_node,
+        # servo_left_shoulder_roll_node,
+        # servo_left_arm_yaw_node,
+        # servo_left_elbow_pitch_node,
+        # servo_left_forearm_yaw_node,
+        # servo_left_wrist_pitch_node,
+        # servo_left_wrist_roll_node,
+        # servo_right_shoulder_pitch_node,
+        # servo_right_shoulder_roll_node,
+        # servo_right_arm_yaw_node,
+        # servo_right_elbow_pitch_node,
+        servo_right_forearm_yaw_node,
+        # servo_right_wrist_pitch_node,
+        # servo_right_wrist_roll_node,
     ]
 
 
