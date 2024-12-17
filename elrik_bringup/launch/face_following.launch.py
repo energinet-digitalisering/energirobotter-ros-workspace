1427bbf1e82eba2878275cd58b08ec9ea8c7340e
Nicoline L. Thomsen <xnlth@energinet.dk>
2024-12-17 13:53:23 +0100

    Added argument to not run face_detection on machine

--- elrik_bringup/launch/face_following.launch.py
+++ elrik_bringup/launch/face_following.launch.py
@@ -22,6 +22,7 @@ def launch_setup(context, *args, **kwargs):
     use_mock_camera = LaunchConfiguration("use_mock_camera")
     use_compressed = LaunchConfiguration("use_compressed")
     camera_model = LaunchConfiguration("camera_model")
+    run_inference_local = LaunchConfiguration("run_inference_local")
 
     image_w = 640
     image_h = 360
@@ -111,6 +112,7 @@ def launch_setup(context, *args, **kwargs):
             {"use_compressed": use_compressed},
             {"box_size_multiplier": 1.5},
         ],
+        condition=IfCondition(run_inference_local),
     )
 
     face_following_node = Node(
@@ -199,6 +201,12 @@ def generate_launch_description():
                 default_value="zedm",
                 description="StereoLabs camera model.",
                 choices=["zedm", "zed2i"],
+            ),
+            DeclareLaunchArgument(
+                "run_inference_local",
+                default_value="true",
+                description="If face_detection is run on this machine, or run separately on another",
+                choices=["true", "false"],
             ),
             OpaqueFunction(function=launch_setup),
         ]
