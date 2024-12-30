26612535198b69951aa7d366ec600f336648c210
Nicoline L. Thomsen <xnlth@energinet.dk>
2024-12-30 16:18:45 +0100

    Rewrote launch file to match the format of others

--- elrik_bringup/launch/ik_test.launch.py
+++ elrik_bringup/launch/ik_test.launch.py
@@ -1,31 +1,77 @@
 from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    OpaqueFunction,
+)
+from launch.conditions import IfCondition
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+package_name = "elrik_bringup"
+
+
+def launch_setup(context, *args, **kwargs):
+    rviz = LaunchConfiguration("rviz")
+
+    urdf_file = PathJoinSubstitution(
+        [
+            FindPackageShare("elrik_description"),
+            "urdf",
+            "phobos_generated.urdf",
+        ]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare("elrik_description"),
+            "rviz",
+            "elrik_display.rviz",
+        ]
+    )
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        arguments=[urdf_file],
+        output="screen",
+    )
+
+    ik_node = Node(
+        package="elrik_kdl_kinematics",
+        executable="elrik_kdl_kinematics_node",
+        output="screen",
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(rviz),
+    )
+
+    return [
+        robot_state_pub_node,
+        ik_node,
+        rviz_node,
+    ]
 
 
 def generate_launch_description():
+
     return LaunchDescription(
         [
-            Node(
-                package="robot_state_publisher",
-                executable="robot_state_publisher",
-                arguments=[
-                    "install/elrik_description/share/elrik_description/urdf/phobos_generated.urdf"
-                ],
-                output="screen",
+            DeclareLaunchArgument(
+                "rviz",
+                default_value="true",
+                description="Start RViz2 automatically with this launch file.",
+                choices=["true", "false"],
             ),
-            Node(
-                package="rviz2",
-                executable="rviz2",
-                arguments=[
-                    "-d",
-                    "install/elrik_description/share/elrik_description/rviz/elrik_display.rviz",
-                ],
-                output="screen",
-            ),
-            Node(
-                package="elrik_kdl_kinematics",
-                executable="elrik_kdl_kinematics_node",
-                output="screen",
-            ),
+            OpaqueFunction(function=launch_setup),
         ]
     )
