dac373908a622bc5773948a76eead1c552fd679e
Nicoline L. Thomsen <xnlth@energinet.dk>
2025-01-16 15:56:19 +0100

    Changed ElrikDriverArms to use base-class

--- pkgs_control/servo_control/servo_control/servo_manager_node.py
+++ pkgs_control/servo_control/servo_control/servo_manager_node.py
@@ -23,8 +23,9 @@ class ServoManagerNode(Node):
         )
 
         # Subscriptions
-        self.subscription = self.create_subscription(
-            JointState, "/joint_states", self.callback_joint_states, 1
+        self.sub_joints_arms = self.create_subscription(
+            JointState, "/joint_states", self.callback_joints_arms, 1
+        )
         )
 
         # Publishers
@@ -35,18 +36,23 @@ class ServoManagerNode(Node):
         # Node variables
         config_folder_path = "install/elrik_bringup/share/elrik_bringup/config/servos"
 
-        self.driver_arms = ElrikDriverArms(config_folder_path, self.control_frequency)
+        # Configure arm servo manager
+        json_files_arms = [
+            f"{config_folder_path}/servo_arm_left_params.json",
+            f"{config_folder_path}/servo_arm_right_params.json",
+        ]
+        self.driver_arms = ElrikDriverArms(json_files_arms, self.control_frequency)
+        self.servo_commands_arms = self.driver_arms.get_default_servo_commands()
 
-        self.servo_commands = self.driver_arms.get_default_servo_commands()
 
-    def callback_joint_states(self, msg):
-        self.servo_commands = dict(zip(msg.name, np.rad2deg(msg.position)))
+    def callback_joints_arms(self, msg):
+        self.servo_commands_arms = dict(zip(msg.name, np.rad2deg(msg.position)))
 
         # self.get_logger().info(f"Updated joint positions: {joint_positions}")
 
     def callback_timer(self):
         self.driver_arms.update_feedback()
-        self.driver_arms.command_servos(self.servo_commands)
+        self.driver_arms.command_servos(self.servo_commands_arms)
 
 
 def main(args=None):
