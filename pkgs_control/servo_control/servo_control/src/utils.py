f9277c027248d6e7916e92dfd5a65c0ef178a0f6
Nicoline L. Thomsen <xnlth@energinet.dk>
2025-01-31 11:22:17 +0100

    Mapped flipped angle to correct range (using angle_software_min/max)

--- pkgs_control/servo_control/servo_control/src/utils.py
+++ pkgs_control/servo_control/servo_control/src/utils.py
@@ -4,4 +4,10 @@ def interval_map(x, x0, x1, y0, y1):
     # [x0, x1]: original interval
     # [y0, y1]: target interval
 
+    if x0 == x1:
+        return y0  # Avoid division by zero; return y0 by default
+
+    # Clamp x within [x0, x1] to avoid out-of-bounds mapping
+    x = max(min(x, x1), x0)
+
     return ((y0 * (x1 - x)) + (y1 * (x - x0))) / (x1 - x0)
