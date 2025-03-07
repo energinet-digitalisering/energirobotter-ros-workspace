c48da4aafdfa7acd060084baae8605457a7ef892
Nicoline L. Thomsen <xnlth@energinet.dk>
2025-03-07 15:19:44 +0100

    Made it easier to switch between I2C busses

--- pkgs_control/servo_control/servo_control/servo_driver_pca9685.py
+++ pkgs_control/servo_control/servo_control/servo_driver_pca9685.py
@@ -4,6 +4,7 @@ Node to test servos connected to a PCA9685.
 
 from adafruit_pca9685 import PCA9685
 import board
+import busio
 
 import rclpy
 from rclpy.node import Node
@@ -24,12 +25,13 @@ class ServoDriverPCA9685(Node):
         # Port Setup
         self.get_logger().info("Initializing I2C communication with PCA9685...")
         try:
-            i2c = board.I2C()
-            self.pca = PCA9685(i2c)
+            i2c = busio.I2C(board.SCL, board.SDA)
+            # i2c = busio.I2C(board.SCL_1, board.SDA_1)
+            self.pca = PCA9685(i2c, address=0x40)
             self.pca.frequency = 50
             self.get_logger().info("I2C communication with PCA9685 succesful")
-        except:
-            self.get_logger().error("Failed to open port, shutting down node...")
+        except Exception as e:
+            self.get_logger().error(f"Failed to open port: {e}, shutting down node...")
             self.destroy_node()
 
     def callback_servo_command(self, msg):
