3b103a21cb6442adbf244fcfe4e416f5f8f9b207
Nicoline L. Thomsen <elrik@elrik-jetson.com>
2025-06-06 15:42:40 +0200

    Added variable to store temperature value

--- pkgs_control/servo_control/servo_control/src/servo_control.py
+++ pkgs_control/servo_control/servo_control/src/servo_control.py
@@ -79,8 +79,9 @@ class ServoControl:
         self.angle_init = self.default_position
         self.angle = self.angle_init
         self.pwm = self.angle_2_pwm(self.angle)
-        self.time_prev = time.time()
+        self.temperature = 0.0
 
+        self.time_prev = time.time()
         self.error_acc = 0.0
         self.error_prev = 0.0
 
@@ -109,6 +110,16 @@ class ServoControl:
 
         self.pwm = feedback_pwm
         self.angle = self.gearing_in(self.pwm_2_angle(feedback_pwm), self.gear_ratio)
+
+    def set_feedback_temperature(self, feedback_temperature):
+        """
+        Updates the servo's temperature value using external feedback.
+
+        Args:
+            feedback_temperature (int): The measured temperature from the servo's feedback system.
+        """
+
+        self.temperature = feedback_temperature
 
     def controller_PID(self, error, error_acc, error_prev, gain_P, gain_I, gain_D):
         """
